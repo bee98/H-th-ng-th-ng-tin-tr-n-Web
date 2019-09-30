80e6611ff2235cf5f5a36b14b8c18e4987811a0d
bee98 <kenkaio1998@gmail.com>
2019-09-30 15:23:05 +0700

    last

--- lessonThree/app/db/schema.rb
+++ lessonThree/app/db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_30_064252) do
+ActiveRecord::Schema.define(version: 2019_09_30_081614) do
+
+  create_table "microports", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
